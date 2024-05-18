35af5f18d9ba4fca0fef93f2784910fe8dc91859
Eugen Ciur <eugen@papermerge.com>
2024-05-18 07:47:29 +0200

    minor adjustments

--- s3worker/tasks.py
+++ s3worker/tasks.py
@@ -1,26 +1,13 @@
-from pathlib import Path
 from celery import shared_task
 from s3worker.client import upload, delete, add_doc_vers, remove_doc_vers
+from s3worker import constants as const
 
 
-@shared_task(name='s3_worker_upload')
-def upload_task(target: str, keyname: str):
-    upload(
-        target_path=Path(target),
-        object_path=Path(keyname)
-    )
-
-
-@shared_task(name='s3_worker_delete')
-def delete_task(keynames: list[str]):
-    delete(object_paths=[Path(k) for k in keynames])
-
-
-@shared_task(name='s3_worker_add_doc_vers')
+@shared_task(name=const.S3_WORKER_ADD_DOC_VER)
 def add_doc_vers_task(doc_ver_ids: list[str]):
     add_doc_vers(doc_ver_ids)
 
 
-@shared_task(name='s3_worker_remove_doc_vers')
+@shared_task(name=const.S3_WORKER_REMOVE_DOC_VER)
 def remove_doc_vers_task(doc_ver_ids: list[str]):
     remove_doc_vers(doc_ver_ids)
