9d59d39da2fa9b1a58ff9426ce5c3b8cf1d2e853
Eugen Ciur <eugen@papermerge.com>
2024-05-18 08:32:41 +0200

    add pyyaml and celery config

--- s3worker/config.py
+++ s3worker/config.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from functools import lru_cache
 from pydantic_settings import BaseSettings
 
@@ -10,7 +11,7 @@ class Settings(BaseSettings):
     aws_region_name: str | None = None
     papermerge__redis__url: str | None = None
     papermerge__main__media_root: str
-
+    papermerge__main__logging_cfg: Path | None = None
 
 @lru_cache()
 def get_settings():
