448fd5b997cce4c9451a99a3ab84b4e67c03705d
Eugen Ciur <eugen@papermerge.com>
2024-05-28 06:06:42 +0200

    minor fixes

--- s3worker/config.py
+++ s3worker/config.py
@@ -12,7 +12,7 @@ class Settings(BaseSettings):
     papermerge__redis__url: str | None = None
     papermerge__main__media_root: str
     papermerge__main__logging_cfg: Path | None = None
-    papermerge__database__url: str | None = None
+    papermerge__database__url: str = 'sqlite:////db/db.sqlite3'
 
 @lru_cache()
 def get_settings():
