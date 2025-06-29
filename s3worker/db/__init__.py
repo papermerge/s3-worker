db3daf7dbba9c2329207127934ecd52f20b6db78
Eugen Ciur <ciur.eugen@gmail.com>
2025-06-29 13:43:47 +0200

    remove obsolete page preview status fields

--- s3worker/db/__init__.py
+++ s3worker/db/__init__.py
@@ -6,7 +6,6 @@ from .api import (
     get_docs,
     update_doc_img_preview_status,
     get_doc_img_preview_status,
-    update_page_img_preview_status,
     get_doc_ver_from_page,
     get_page_number
 )
@@ -18,7 +17,6 @@ __all__ = [
     'get_pages',
     'update_doc_img_preview_status',
     'get_doc_img_preview_status',
-    'update_page_img_preview_status',
     'get_doc_ver_from_page',
     'get_page_number',
     'User',
--- tests/test_tasks.py
+++
@@ -1,7 +0,0 @@
-import uuid
-from s3worker import tasks
-
-
-def test_generate_page_image_task():
-    uid = uuid.uuid4()
-    tasks.generate_page_image_task(str(uid))
